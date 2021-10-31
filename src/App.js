1db66372fd2ec9eec3d7f3d2f21a27a2a25b32d6
vaibhav <chaudharivaibhav60@gmail.com>
2021-10-31 21:50:58 +0530

    sdsd

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Textform from "./components/textform";
+function App() {
+  return (
+    <div className="App">
+      <Textform />
+    </div>
+  );
+}
+
+export default App;
+// git remote set-url origin https://github.com/vaibhav0060/employee.git
+// git remote set-url origin https://github.com/vaibhav0060/react-3rd-assignment.git
